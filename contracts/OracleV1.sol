c643e5081c2675c1c493e0931f511fb6aec47ff4
pglekshmi <107828565+pglekshmi@users.noreply.github.com>
2024-05-25 10:12:44 +0530

    Update OracleV1.sol

--- contracts/OracleV1.sol
+++ contracts/OracleV1.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.8.19;
 
 contract OracleV1 {
-    string public tempData;
+    string public priceData;
 
-    function setTempData(string memory _tempData) public {
-        tempData = _tempData;
+    function setPriceData(string memory _priceData) public {
+        priceData = _priceData;
     }
 }
